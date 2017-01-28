8d57d4ebba32c4dc3c8d7e93eb36adf77331537d
Tom Limoncelli <tal@whatexit.org>
2017-01-28 09:53:18 -0500

    Rewritten to use a Python3 class.

--- dechain/demo.py
+++ dechain/demo.py
@@ -2,95 +2,135 @@
 
 import sys
 import argparse
-import pprint
 
-global args
+class Chains:
 
-def load_chains():
-    return {
-            # Normal links:
-            'A': 'X',
-            'B': 'Y',
-            'C': 'Z',
-            # Long chain: D->E->F->G->H->I-J
-            'D': 'E',
-            'E': 'F',
-            'F': 'G',
-            'G': 'H',
-            'H': 'I',
-            'I': 'J',
-            # Another chain: O->N->M->L->K
-            'L': 'K',
-            'M': 'L',
-            'N': 'M',
-            'O': 'N',
-            # Infinite loop:
-            'O': 'O',
-            }
+  graph = {}  # key = URL. value = next in chain.
+  changed = set()  # set of which URLs have changed.
 
-def optimize_one_pass(chains):
-    """The 1-pass optimizer.
+  def load_from_api(self):
+    """Loads the data.  Replace this with something that loads from the API."""
+    self.graph = {
+        # Normal links:
+        'A': 'X',
+        'B': 'Y',
+        'C': 'Z',
+        # Long chain: D->E->F->G->H->I-J
+        # (due to ordering, this will require multiple passes)
+        'D': 'E',
+        'E': 'F',
+        'F': 'G',
+        'G': 'H',
+        'H': 'I',
+        'I': 'J',
+        # Another chain: O->N->M->L->K
+        # (due to ordering, this will resolve in 1 pass)
+        'L': 'K',
+        'M': 'L',
+        'N': 'M',
+        'O': 'N',
+        # Infinite loop:
+        'R': 'R',
+    }
+
+  def flatten_once(self):
+    """The 1-pass flattening.
     For any chains that are longer than 1 hop, reduces them by
     at least 1 hop (maybe more).
     """
-    for i in sorted(chains.keys()):
-        n =  chains[i]
-        if n in chains:
-            chains[i] = chains[n]
+    for i in sorted(self.graph.keys()):  # Sorted for stable unit tests.
+      n = self.graph[i]
+      if n in self.graph:
+        self.graph[i] = self.graph[n]
+        self.changed.add(i)
 
-def print_chains(chains):
+  def print_all(self):
     """Print chains."""
-    for j in sorted(chains.keys()):
-        print(j, chains[j], chain_length(chains, j))
+    for k in sorted(self.graph.keys()):
+      if k in self.changed:
+        c = ' * '
+      else:
+        c = '   '
+      print(k, self.graph[k], c, self.chain_length(k), self.chainToString(k))
 
-def count_chains(chains):
-    """Returns the total length of all chains."""
+  def total_length(self):
+    """Returns the sum of the lengths of all chains."""
     total = 0
-    for j in chains.keys():
-        total += chain_length(chains, j)
+    for k in self.graph.keys():
+      total += self.chain_length(k)
     return total
 
-def chain_length(chains, i):
+  def chain_length(self, k):
     """
     Returns the chain length. If the chain is infinite, the return value
-    is 1000000000 plus the step at which the infinite loop began.
+    is the step number at which the infinite loop began.
+    TODO(tlim): Infinite loops should result in an exception.
     """
     seen = {}
-    return chain_length_helper(chains, i, seen)
+    return self._chain_length_helper(k, seen)
 
-def chain_length_helper(chains, i, seen):
-    if i in seen:
-        return 1000000000 - 1
-    seen[i] = True
-    if i in chains:
-        return 1 + chain_length_helper(chains, chains[i], seen)
+  def _chain_length_helper(self, k, seen):
+    if k in seen:
+      return 0
+    seen[k] = True
+    if k in self.graph:
+      return 1 + self._chain_length_helper(self.graph[k], seen)
     else:
-        return 0
+      return 0
+
+  def chainToString(self, k):
+    """
+    Returns the chain as a string like "A->B->C".
+    """
+    seen = {}
+    return self._chainToString_helper(k, seen)
+
+  def _chainToString_helper(self, k, seen):
+    if k in seen:
+      return "REPEATING"
+    seen[k] = True
+    if k in self.graph:
+      return str(k) + "->" + self._chainToString_helper(self.graph[k], seen)
+    else:
+      return str(k)
+
+  def changed_keys(self):
+    """Iterator that reveals the changed items."""
+    for it in sorted(self.graph.items()):
+      if it[0] in self.changed:
+        yield it
+
 
 def main():
   # parse cmd line flags.
   # http://docs.python.org/2/library/argparse.html#the-add-argument-method
   parser = argparse.ArgumentParser()
-  parser.add_argument('-n', '--dry-run', action='store_true')
+  parser.add_argument('--real', '-r', action='store_true')
   args = parser.parse_args()
 
-  chains = load_chains()
-  #pprint.pprint(chains)
+  g = Chains()
+  g.load_from_api()
   print("BEFORE:")
-  print_chains(chains)
+  g.print_all()
 
   passcount = 0
   while True:
-      t_before = count_chains(chains)
-      optimize_one_pass(chains)
-      t_after = count_chains(chains)
-      if t_before == t_after:
-          break
+    t_before = g.total_length()
+    g.flatten_once()
+    t_after = g.total_length()
+    # No change? Must be at the optimal state.
+    if t_before == t_after:
+      break
+    passcount += 1
+    print("PASS:", passcount)
+    g.print_all()
 
-      passcount += 1
-      print("PASS:", passcount)
-      print_chains(chains)
-
+  print("Use the API to make this changes:")
+  for chain in g.changed_keys():
+    if args.real:
+      print(chain, "MAKING CHANGE (not implemented)")
+    else:
+      print(chain, "DEBUG MODE. NOT MAKING CHANGE.")
 
 if __name__ == '__main__':
   sys.exit(main())
