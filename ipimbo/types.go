87ca9e7b2be4f9565f406f91928acb62fdbc8c81
Tom Limoncelli <tal@whatexit.org>
2017-12-14 14:16:18 -0500

    save

---
+++ ipimbo/types.go
@@ -0,0 +1,21 @@
+package ipimbo
+
+import (
+	"fmt"
+	"net"
+)
+
+type Imbo struct {
+	version    int
+	addr       net.IP
+	prefixlen  uint8
+	isIP       bool
+	isZeroAddr bool
+}
+
+func (b *Imbo) String() string {
+	if b.isIP {
+		return b.addr.String()
+	}
+	return fmt.Sprintf("%s/%s", b.addr, b.prefixlen)
+}
