fc3348463a1cfdf27c10a5a7ca143ffc6d789b77
Tom Limoncelli <tal@whatexit.org>
2023-08-13 11:28:42 -0400

    Generate file0.txt too

--- mkfiledirlist/main.go
+++ mkfiledirlist/main.go
@@ -31,28 +31,55 @@ func main() {
 		isDir[d] = true
 	}
 
+	// BUG: Empty directories are listed as isDir == false.
+
 	// scan the list, outputting each item in all.txt, files.txt or dirs.txt
 	ao, err := os.Create("all.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer ao.Close()
+
 	fo, err := os.Create("files.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer fo.Close()
+
+	fo0, err := os.Create("files0.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fo0.Close()
+
 	do, err := os.Create("dirs.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer do.Close()
+
+	do0, err := os.Create("dirs0.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer do0.Close()
+
 	for _, name := range fileNames {
+
 		fmt.Fprintln(ao, name)
+
 		if isDir[name] {
 			fmt.Fprintln(do, name)
+
+			fmt.Fprint(do0, name)
+			fmt.Fprint(do0, "\000")
+
 		} else {
 			fmt.Fprintln(fo, name)
+
+			fmt.Fprint(fo0, name)
+			fmt.Fprint(fo0, "\000")
+
 		}
 	}
 
